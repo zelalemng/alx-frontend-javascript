d4b024f366c985a973c9273616a41a5aed4f2332
zelalemng <zedneg@gmail.com>
2024-03-05 20:35:33 +0000

    tests_tasks

---
+++ 0x00-ES6_basic/tests/test.js
@@ -0,0 +1,22 @@
+package.json
+{
+ "name": "0x00-ES6_basic",
+ "version": "1.0.0",
+ "description": "",
+ "main": "main.js",
+ "scripts": 
+  {
+   "test": "jest --verbose"
+  },
+ "keywords": [],
+ "author": "Name Oner",
+ "license": "ISC",
+ "devDependencies":
+  {
+   "jest": "^24.0.0"
+  },
+ "dependencies": 
+  {
+   "axios": "^0.18.0"
+  }
+}
